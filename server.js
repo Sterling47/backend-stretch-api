5e1e6bd1a3dab8ed48a98e72034aa19c9c9a5923
Zach Wolek <zach.wolek@gmail.com>
2024-07-23 11:43:32 -0500

    feat: add route for food category

--- server.js
+++ server.js
@@ -11,11 +11,21 @@ const fs = require('node:fs');
 const {finished} = require('stream/promises');
 
 
-app.get('/', async (req,res) => {
- 
-      res.status(500).json({error});
-
+app.get('/foodcategory/:id', async (req,res) => {
+    try {
+        const foodCategory = await database('branded_food_table as b').select('b.fdc_id', 'b.brand_name', 'b.ingredients', 'b.serving_size', 'b.serving_size_unit', 'b.branded_food_category')
+            .where('b.fdc_id', '=', req.params.id)
+            res.status(200).json(foodCategory.slice(0, 100))
+    } catch(error) {
+        res.status(500).json({error})
+    } 
 })
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
 })
+
+// Columns: fdc_id, brand_name, 
+//(from food_table) 
+//description, ingredients, serving_size, serving_size_unit, branded_food_category
