df4882e26edf0ac0c001463da5ff6c03bd35e2b6
Peter Kim <peterkim.pk1@gmail.com>
2024-07-23 20:25:09 -0400

    Created route to get foods based on category

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const app = express();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const cors = require('cors');
+app.use(cors());
 
-const {parse} = require('csv-parse');
-const fs = require('node:fs');
-const {finished} = require('stream/promises');
-
-
+app.get('/foodcategory/:category', async (req,res) => {
+    try {
+        const filteredFoods = await database('branded_food_table as b')
+            .select('b.fdc_id', 'b.brand_name', 'b.ingredients', 'b.serving_size', 'b.serving_size_unit', 'b.branded_food_category')
+            .where('b.branded_food_category', '=', req.params.category)
+            .join('food_table as f', 'b.fdc_id', 'f.fdc_id').select('b.fdc_id', 'b.brand_name', 'b.ingredients', 'b.serving_size', 'b.serving_size_unit', 'b.branded_food_category', 'f.description')
+            .whereRaw('f.fdc_id = b.fdc_id')
+            res.status(200).json(filteredFoods.slice(0, 100))
+    } catch(error) {
+        res.status(500).json({error})
+    } 
+})
 app.get('/foodcategory/:id', async (req,res) => {
     try {
         const foodCategory = await database('branded_food_table as b')
